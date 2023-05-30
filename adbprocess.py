4b216bdfa2391241bc6b8dc2dba4b5018cc7b0e0
Jhonatha de Sodre <108944889+desodre@users.noreply.github.com>
2023-05-30 17:57:08 -0400

    Update Adb_Simplify.py

--- adbprocess.py
+++ adbprocess.py
@@ -1,29 +1,39 @@
 import subprocess
-
-class adb_device:
-        
-    output = subprocess.check_output('adb devices', shell=True).decode('utf-8')
-    lines = output.strip().split('\n')[1:]
-    devices = [line.split('\t')[0] for line in lines]
-    
-    def __init__(self, index):
-        self.index = index
-
-    def __str__(self):
-        if self.index != None:
-            return self.devices[self.index]
-        else:
-            print(f'all devices: {self.devices}')
+import time
 
 
-def open_activity(activity, dispositivo = None):
-    if dispositivo == None:
-        subprocess.call(f'adb shell am start -a {activity}')
-    else:
-        subprocess.call(f'adb {dispositivo} shell am start -a {activity}')
+class Adb_Simplify:
+    @staticmethod
+    def devices():
+        command = ['adb', 'devices']
+        result = subprocess.run(command, capture_output=True, text=True)
+        output = result.stdout.strip().split('\n')[1:]
+        devices = [line.split('\t')[0] for line in output if line.endswith('\tdevice')]
+        return devices
 
+    @staticmethod
+    def screenshot(device_id, save_path):
+        command = f"adb -s {device_id} shell screencap -p"
+        result = subprocess.run(command, capture_output=True)
+        with open(save_path, 'wb') as file:
+            file.write(result.stdout)
 
-def phantomTouch(x ,y):
-    subprocess.call(f"adb shell input tap {x} {y}")
+    @staticmethod
+    def start_activity(device_id, package, activity):
+        command = f"adb -s {device_id} shell am  start -n {package}/{activity}"
+        subprocess.run(command)
 
-    
+    @staticmethod
+    def click(device_id, x, y):
+        command = f"adb -s {device_id} shell input tap {str(x)} {str(y)}"
+        subprocess.run(command)
+
+    @staticmethod
+    def call(device_id, number, duracao=3):
+        subprocess.run(f"adb -s {device_id} shell am start -a android.intent.action.DIAL")
+        time.sleep(1)
+        subprocess.run(f"adb -s {device_id} shell input text {number}")
+        subprocess.run(f"adb -s {device_id} shell input keyevent 5")
+        time.sleep(duracao)
+        subprocess.run(f"adb -s {device_id} shell input keyevent 6")
+
