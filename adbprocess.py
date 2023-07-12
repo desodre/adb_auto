e0f1bc718fa99169c03f6e33c5c2178a89273465
Jhonatha de Sodre <108944889+desodre@users.noreply.github.com>
2023-07-12 15:04:39 -0400

    AndroidSubprocess.py

--- adbprocess.py
+++ adbprocess.py
@@ -1,39 +1,69 @@
-import subprocess
-import time
+import subprocess as sb
+import os
+import xml.etree.ElementTree as ET
+
+#demorei nisso aqui viu, mizera
+
+def Devices():
+    command = ['adb', 'devices']
+    result = sb.run(command, capture_output=True, text=True)
+    output = result.stdout.strip().split('\n')[1:]
+    devices = [line.split('\t')[0] for line in output if line.endswith('\tdevice')]
+    return devices
 
 
-class Adb_Simplify:
-    @staticmethod
-    def devices():
-        command = ['adb', 'devices']
-        result = subprocess.run(command, capture_output=True, text=True)
-        output = result.stdout.strip().split('\n')[1:]
-        devices = [line.split('\t')[0] for line in output if line.endswith('\tdevice')]
-        return devices
+def Screenshot(devices: list, nome: str):
+    for device in devices:
+        local = sb.check_output(f"adb -s {device} ls sdcard/DCIM")
 
-    @staticmethod
-    def screenshot(device_id, save_path):
-        command = f"adb -s {device_id} shell screencap -p"
-        result = subprocess.run(command, capture_output=True)
-        with open(save_path, 'wb') as file:
-            file.write(result.stdout)
+        #Aqui ele verifica se a pagina ja existe, se nao ele cria e tira o print do msm jeito
 
-    @staticmethod
-    def start_activity(device_id, package, activity):
-        command = f"adb -s {device_id} shell am  start -n {package}/{activity}"
-        subprocess.run(command)
+        if "adbScrenshots" in str(local):
+            sb.run(f"adb -s {device} shell screencap sdcard/DCIM/adbScrenshots/{nome}.png")
+            print("Print tirado com sucesso")
+        else:
+            print("Pasta adbScrenshots nao encontrada, sera criada atumaticamente")
+            sb.run(f"adb -s {device} shell mkdir sdcard/DCIM/adbScrenshots")
+            sb.run(f"adb -s {device} shell screencap sdcard/DCIM/adbScrenshots/{nome}.png")
+            print("Print tirado com sucesso")
 
-    @staticmethod
-    def click(device_id, x, y):
-        command = f"adb -s {device_id} shell input tap {str(x)} {str(y)}"
-        subprocess.run(command)
+def Click(devices: list, x: int, y: int):
+    for device in devices:
+        sb.run(f"adb -s {device} shell input tap {x} {y}")
 
-    @staticmethod
-    def call(device_id, number, duracao=3):
-        subprocess.run(f"adb -s {device_id} shell am start -a android.intent.action.DIAL")
-        time.sleep(1)
-        subprocess.run(f"adb -s {device_id} shell input text {number}")
-        subprocess.run(f"adb -s {device_id} shell input keyevent 5")
-        time.sleep(duracao)
-        subprocess.run(f"adb -s {device_id} shell input keyevent 6")
+
+def getDeviceInfos(devices: list):
+    for device in devices:
+        model = (sb.run(f"adb -s {device} shell getprop ro.product.model", capture_output=True, text=True).stdout).strip()
+        activated_id = (sb.run(f"adb -s {device} shell getprop ro.boot.activatedid", capture_output=True, text=True).stdout).strip()
+
+        if os.path.exists(f"Projetos\{model}"):
+            pass
+        else:
+            os.mkdir(f"{model}")
+        
+        if os.path.exists(f"Projetos\{model}\{activated_id}"):
+            pass
+        else:
+            os.mkdir(f"Projetos\{model}\{activated_id}")
+
+        os.system(f"adb -s {device} shell getprop >Projetos\{model}\{activated_id}\PropriedadesDispositivo.txt ")
+
+def getIMEI(devices: list):
+    for device in devices:
+        sb.run(f"adb -s {device} shell input keyevent KEYCODE_CALL")
+        sb.run(f"adb -s {device} shell input text '*#06#'")
+        sb.run(f"adb -s {device} shell  uiautomator dump --compressed")
+        sb.run(f"adb -s {device} pull /sdcard/window_dump.xml")
+
+        with open("window_dump.xml", "r") as arquivo:
+            primeira_linha = arquivo.readlines()
+
+        root = ET.fromstring(primeira_linha)
+        imei1_node = root.find(".//node[@text='IMEI1']")
+        imei1 = imei1_node.tail.strip()
+        print("IMEI1:", imei1)
+        
+
+getIMEI(Devices())
 
